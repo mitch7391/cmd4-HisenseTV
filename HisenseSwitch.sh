a75054dc6e3aaad15487f85847171479d56d07e6
Mitch Williams <40288237+mitch7391@users.noreply.github.com>
2021-01-23 14:21:36 +0800

    Make HisenseSwitch.sh more user friendly.
    IP Address and both MAC Addresses only need to be edited at the top of the script now; less chance for new users to make a mistake.

--- HisenseSwitch.sh
+++ HisenseSwitch.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# IP Address of TV (go to TV settings to find this).
 ip="192.168.0.156"
-port="36669"
+# MAC Address of the TV (go to TV settings to find this).
+tvMAC="7C:B3:7B:76:77:D4"
+# MAC Address of device paired to the TV through the RemoteNOW app.
+pairedMAC="8C:84:01:20:57:18"
 
 if [ "$1" = "Get" ]; then
   case "$3" in
 
     On )
-      if [ $(timeout 2 /bin/bash -c "(echo > /dev/tcp/192.168.0.156/36669) > /dev/null 2>&1 && echo 1 || echo 0")  = '1' ]; then
+      # Polls the TV over your network to see if it is still active.
+      if [ $(timeout 2 /bin/bash -c "(echo > /dev/tcp/"$ip"/36669) > /dev/null 2>&1 && echo 1 || echo 0")  = '1' ]; then
         echo 1
       else
         echo 0
@@ -22,11 +27,11 @@ if [ "$1" = "Set" ]; then
 
     On )
       if [ "$4" = "true" ]; then
-        #TV can only be turned back on by WOL using your TV MAC ADDRESS
-        wakeonlan XX:XX:XX:XX:XX:XX
+        # TV can only be turned back on by WOL using your TV MAC ADDRESS.
+        wakeonlan "$tvMAC"
       else
-        #TV can turn off using MQTT
-        mosquitto_pub --cafile /home/pi/hisense.crt --insecure -h $ip -p $port -P multimqttservice -u hisenseservice -t "/remoteapp/tv/remote_service/XX:XX:XX:XX:XX:XX$normal/actions/sendkey" -m "KEY_POWER"
+        # TV can turn off using MQTT.
+        mosquitto_pub --cafile /home/pi/hisense.crt --insecure -h $ip -p 36669 -P multimqttservice -u hisenseservice -t "/remoteapp/tv/remote_service/"$pairedMAC"$normal/actions/sendkey" -m "KEY_POWER"
       fi
       ;;
 
